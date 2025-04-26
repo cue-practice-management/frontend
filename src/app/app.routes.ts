e886823c48e7c91c3bed9cd7b87ecf7a254aba08
Juan Gerardo <jmendez1025@cue.edu.co>
2025-04-25 22:08:13 -0500

    chore(app): update bootstrap configuration and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { HomePageComponent } from './features/homes/pages/home-page/home-page.component';
+import { AUTH_PREFFIX } from './core/constants/routes.constants';
+import { MainLayoutComponent } from './shared/components/layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
     {
@@ -10,7 +11,12 @@ export const routes: Routes = [
             {
                 path: '',
                 component: HomePageComponent
-            }
+            },
+
         ]
+    },
+    {
+        path: AUTH_PREFFIX,
+        loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
     }
 ];
