fec3b4efc9adac98f16defc98ece64a2e32501f8
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-12 14:20:44 -0500

    feat: spinner atom

--- src/app/shared/components/organisms/dynamic-form/dynamic-form.component.ts
+++ src/app/shared/components/organisms/dynamic-form/dynamic-form.component.ts
@@ -5,13 +5,14 @@ import { CommonModule } from '@angular/common';
 import { InputFormRowComponent } from '../../molecules/input-form-row/input-form-row.component';
 import { DynacmicFormConfig } from './dynamic.form.models';
 import { ButtonComponent } from "../../atoms/button/button.component";
+import { SpinnerComponent } from '../../atoms/spinner/spinner.component';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
   styleUrls: ['./dynamic-form.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, InputFormRowComponent, ButtonComponent],
+  imports: [ReactiveFormsModule, CommonModule, InputFormRowComponent, ButtonComponent, SpinnerComponent],
 })
 export class DynamicFormComponent<T> implements OnInit {
   @Input() dynamicFormConfig!: DynacmicFormConfig;
@@ -43,9 +44,7 @@ export class DynamicFormComponent<T> implements OnInit {
     return this.form.get(key) as FormControl;
   }
 
-  get buttonLabel(): string {
-    if(this.isLoading) return 'Cargando...';
-    
+  get buttonLabel(): string {    
     return this.dynamicFormConfig.buttonLabel || 'Enviar';
   }
 }
