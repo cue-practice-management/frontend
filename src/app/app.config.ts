8c556b9a1976f4ec6f3a22d3f8b22ad1d6738425
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-13 09:53:28 -0500

    fix: init when user not auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, ApplicationConfig,  inject } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, inject } from '@angular/core';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './features/auth/interceptors/auth.interceptor';
@@ -8,27 +8,14 @@ import { catchError, firstValueFrom, of } from 'rxjs';
 import { AuthService } from './features/auth/services/auth.service';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations'; 
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 function initializeApp(): () => Promise<void> {
-  const authService = inject(AuthService);
   const currentUserService = inject(CurrentUserService);
 
   return async () => {
     try {
-      const refresh = await firstValueFrom(
-        authService.refreshToken().pipe(
-          catchError(() => {
-            return of(null); 
-          })
-        )
-      );
-
-      if (refresh?.accessToken) {
-        await currentUserService.loadCurrentUser();
-      } else {
-        currentUserService.clear(); 
-      }
+      await currentUserService.loadCurrentUser();
     } catch (e) {
       currentUserService.clear();
     }
