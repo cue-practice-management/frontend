d865d2886f140a8dffe0c1c279c4b5f5f1d0a540
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-21 22:41:43 -0500

    feat(admin): soporte para filtros dinámicos en AdminEntityPage

--- src/app/features/admin/admin.constants.ts
+++ src/app/features/admin/admin.constants.ts
@@ -3,6 +3,7 @@ import { SidebarItem } from "./admin.models";
 import * as LucideIcons from 'lucide-angular';
 
 export const ADMIN_SIDEBAR_ITEMS: SidebarItem[] = [
+    { label: 'Estudiantes', route: ROUTES.ADMIN.STUDENTS, icon: LucideIcons.User },
     { label: 'Facultades', route: ROUTES.ADMIN.FACULTIES, icon: LucideIcons.SwatchBook },
     { label: 'Programas Academicos', route: ROUTES.ADMIN.ACADEMIC_PROGRAMS, icon: LucideIcons.BookType },
     { label: 'Equipos', route: '/admin/teams', icon: LucideIcons.Users },
--- src/app/features/admin/components/admin-entity-page/admin-entity-page.component.ts
+++ src/app/features/admin/components/admin-entity-page/admin-entity-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectorRef, Component, ComponentRef, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ComponentRef, EnvironmentInjector, Input, OnDestroy, Type, ViewChild, ViewContainerRef } from '@angular/core';
 import { AdminSectionWrapperComponent } from '../admin-section-wrapper/admin-section-wrapper.component';
 import { ButtonComponent } from '@/shared/components/atoms/button/button.component';
 import { AdminEntityPageConfig } from '../../admin.models';
@@ -7,6 +7,7 @@ import { ModalService } from '@/core/services/modal.service';
 import { TableAction } from '@/shared/models/table-actions.enum';
 import { PaginationQuery } from '@/core/models/pagination-query.model';
 import { DataTable } from '@/shared/abstracts/data-table.abstract';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-entity-page',
@@ -14,39 +15,70 @@ import { DataTable } from '@/shared/abstracts/data-table.abstract';
   imports: [CommonModule, AdminSectionWrapperComponent, ButtonComponent],
   templateUrl: './admin-entity-page.component.html',
 })
-export class AdminEntityPageComponent<
-  T extends { _id: string },
-  U extends PaginationQuery = PaginationQuery
-> implements AfterViewInit {
+export class AdminEntityPageComponent<T extends { _id: string }, U extends PaginationQuery = PaginationQuery> implements AfterViewInit, OnDestroy {
   @Input() config!: AdminEntityPageConfig<T, U>;
 
   @ViewChild('tableContainer', { read: ViewContainerRef, static: true })
   tableContainer!: ViewContainerRef;
 
-  tableInstance!: ComponentRef<DataTable<T, U>>;
+  @ViewChild('filterContainer', { read: ViewContainerRef, static: true }) 
+  filterContainer!: ViewContainerRef;
 
-  constructor(private modalService: ModalService,   private cdr: ChangeDetectorRef) {}
+  tableInstance!: ComponentRef<DataTable<T, U>>;
+  private filterSub?: Subscription;
+
+  constructor(
+    private modalService: ModalService,
+    private cdr: ChangeDetectorRef,
+    private envInjector: EnvironmentInjector
+  ) { }
 
   ngAfterViewInit(): void {
-    const { tableComponent, allowedActions = [TableAction.EDIT, TableAction.DELETE] } = this.config;
-
-    this.tableContainer.clear();
-    this.tableInstance = this.tableContainer.createComponent(tableComponent);
-    this.tableInstance.instance.allowedActions = allowedActions;
-
+    this.initTableComponent();
+    this.initFilterComponent();
     this.cdr.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.filterSub?.unsubscribe();
   }
 
   openCreateEntityModal(): void {
     const { formComponent, modalData = {} } = this.config;
     const data = typeof modalData === 'function' ? modalData() : modalData;
 
-    this.modalService.open(formComponent, { data }).afterClosed()
+    this.modalService.open(formComponent, { width: this.config.modalWidth, data }).afterClosed()
       .subscribe(result => {
         if (result) {
           this.tableInstance.instance.reloadPageData();
         }
       });
   }
-}
 
+  private initTableComponent(): void {
+    const { tableComponent, allowedActions = [TableAction.EDIT, TableAction.DELETE] } = this.config;
+    this.tableContainer.clear();
+    this.tableInstance = this.tableContainer.createComponent(tableComponent, {
+      environmentInjector: this.envInjector
+    });
+    this.tableInstance.instance.allowedActions = allowedActions;
+    this.cdr.detectChanges();
+  }
+
+  private initFilterComponent(): void {
+    const { filterComponent } = this.config;
+    if (!filterComponent) return;
+    
+    this.filterContainer.clear();
+    const filterRef = this.filterContainer.createComponent(filterComponent, {
+      environmentInjector: this.envInjector
+    });
+    
+    this.filterSub?.unsubscribe();
+    this.filterSub = filterRef.instance.filterChange.subscribe((filter: U) => {
+      if (this.tableInstance?.instance?.loadPageData) {
+        this.tableInstance.instance.loadPageData(filter);
+      }
+    });
+  }
+}
