e17666134d7324c598f346db3811b858dbce703f
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-22 11:02:53 -0500

    fix(modal): stop click propagation

--- src/app/shared/components/atoms/input-select/input-select.component.ts
+++ src/app/shared/components/atoms/input-select/input-select.component.ts
@@ -1,19 +1,53 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, HostListener, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { SelectOption } from './input-select.models';
-import { CommonModule } from '@angular/common';
+import { ClickOutsideDirective } from '@/shared/directives/click-outside.directive';
 
 @Component({
   selector: 'app-input-select',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ClickOutsideDirective],
   templateUrl: './input-select.component.html',
   styleUrl: './input-select.component.scss'
 })
-export class InputSelectComponent {
+export class InputSelectComponent implements OnInit, OnChanges {
   @Input() control!: FormControl;
   @Input() id = '';
-  @Input() placeholder = '';
+  @Input() placeholder = 'Seleccionar...';
   @Input() options: SelectOption[] = [];
+
+  showDropdown = false;
+  labelSelected = '';
+
+  ngOnInit(): void {
+    this.setLabelSelected();
+  }
+
+  ngOnChanges(): void {
+    this.setLabelSelected();
+  }
+
+  toggleDropdown(): void {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  selectOption(option: SelectOption): void {
+    this.labelSelected = option.label;
+    this.control.setValue(option.value);
+    this.showDropdown = false;
+  }
+
+  closeDropdown(): void {
+    this.showDropdown = false;
+  }
+
+  get hasInitialValue(): boolean {
+    return !!this.labelSelected;
+  }
+  private setLabelSelected(): void {
+    const selected = this.options.find(o => o.value === this.control.value);
+    if (selected) this.labelSelected = selected.label;
+  }
 
 }
