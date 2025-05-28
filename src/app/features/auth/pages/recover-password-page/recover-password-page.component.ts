38615914b4f3b0a4376c40172f0fb871ae46b73b
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-28 17:51:59 -0500

    feat(auth): add recover password page and service integration

--- src/app/features/auth/pages/recover-password-page/recover-password-page.component.ts
+++ src/app/features/auth/pages/recover-password-page/recover-password-page.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { DynamicFormComponent } from '@/shared/components/organisms/dynamic-form/dynamic-form.component';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormSubmitComponent } from '@/shared/abstracts/form-submit.abstract';
+import { RecoverPasswordRequestDto } from '../../models/recover-password-request.dto';
+import { DynacmicFormConfig } from '@/shared/components/organisms/dynamic-form/dynamic.form.models';
+import { FormFieldType } from '@/shared/models/form-field-type.enum';
+import { Validators } from '@angular/forms';
+import { RECOVER_EMAIL_KEY } from '../../auth.constants';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { ROUTES } from '@/core/constants/routes.constants';
+import { AppAuthCardComponent } from "../../components/auth-card/auth-card.component";
+import { LockIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-recover-password-page',
   standalone: true,
-  imports: [],
+  imports: [DynamicFormComponent, AppAuthCardComponent],
   templateUrl: './recover-password-page.component.html',
-  styleUrl: './recover-password-page.component.scss'
+  styleUrls: ['./recover-password-page.component.scss'],
 })
-export class RecoverPasswordPageComponent {
+export class RecoverPasswordPageComponent extends FormSubmitComponent<RecoverPasswordRequestDto, void> {
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    super();
+  }
+
+
+  recoverPasswordFormConfig: DynacmicFormConfig = {
+    title: '',
+    sections: [
+      {
+        fields: [
+          {
+            key: 'email',
+            label: 'Correo electrónico',
+            type: FormFieldType.TEXT,
+            placeholder: 'ejemplo@correo.com',
+            validators: [Validators.required, Validators.email],
+          }
+        ]
+      }
+    ]
+  };
+
+  onRecoverPasswordSubmit(formValue: RecoverPasswordRequestDto): void {
+    this.submitForm(formValue);
+  }
+
+  protected override successMessage: string = 'Hemos enviado un correo electrónico con las instrucciones para recuperar tu contraseña.';
+
+  override submitData(dto: RecoverPasswordRequestDto): any {
+    return this.authService.recoverPassword(dto).pipe(
+      tap(() => {
+        localStorage.setItem(RECOVER_EMAIL_KEY, dto.email);
+        this.router.navigate([ROUTES.AUTH.RECOVER_PASSWORD_VALIDATE], {
+          queryParams: { email: dto.email },
+        });
+      })
+    );
+  }
+
+  LockIcon = LockIcon;
 }
