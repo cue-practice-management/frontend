326971fe9970452790964b9ff69a11843f0be946
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-08 15:59:02 -0500

    feat(admin): enable filtering in student-company-contract admin page

--- src/app/features/admin/pages/admin-student-company-contract-page/admin-student-company-contract-page.component.ts
+++ src/app/features/admin/pages/admin-student-company-contract-page/admin-student-company-contract-page.component.ts
@@ -5,6 +5,7 @@ import { StudentCompanyContract, StudentCompanyContractFilter } from '@/features
 import { StudentCompanyContractTableComponent } from '@/features/student-company-contract/components/student-company-contract-table/student-company-contract-table.component';
 import { StudentCompanyContractFormComponent } from '@/features/student-company-contract/components/student-company-contract-form/student-company-contract-form.component';
 import { TableAction } from '@/shared/models/table-actions.enum';
+import { StudentCompanyContractFilterComponent } from '@/features/student-company-contract/components/student-company-contract-filter/student-company-contract-filter.component';
 
 @Component({
   selector: 'app-admin-student-company-contract-page',
@@ -19,6 +20,7 @@ export class AdminStudentCompanyContractPageComponent {
     description: 'Gestiona los contratos entre estudiantes y empresas',
     createButtonLabel: 'Crear contrato',
     tableComponent: StudentCompanyContractTableComponent,
+    filterComponent: StudentCompanyContractFilterComponent,
     formComponent: StudentCompanyContractFormComponent,
     allowedActions: [TableAction.DELETE],
     modalWidth: '800px',
