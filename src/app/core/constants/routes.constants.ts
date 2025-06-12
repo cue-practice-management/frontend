c819ab31dfc18df6447f37289504178dcf743ec4
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-12 13:28:51 -0500

    fix(routes): correct practice definition route path in constants

--- src/app/core/constants/routes.constants.ts
+++ src/app/core/constants/routes.constants.ts
@@ -23,7 +23,7 @@ export const ROUTES = {
         NEWS: `${ADMIN_PREFFIX}/news`,
         STUDENT_COMPANY_CONTRACTS: `${ADMIN_PREFFIX}/student-company-contracts`,
         PRACTICE_TEMPLATES: `${ADMIN_PREFFIX}/practice-templates`,
-        PRACTICE_DEFINITION:  `${ADMIN_PREFFIX}/practice-definition`
+        PRACTICE_DEFINITION:  `${ADMIN_PREFFIX}/practice-definitions`
     },
 }
 
