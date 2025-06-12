fa07872dc4b4852ad82d026d495d60db408ac5ab
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-12 09:51:04 -0500

    feat(home-page): update layout and integrate hero section

--- src/app/features/homes/pages/home-page/home-page.component.ts
+++ src/app/features/homes/pages/home-page/home-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from "../../components/hero-section/hero-section.component";
+import { AboutSectionComponent } from "../../components/about-section/about-section.component";
+import { FeaturesSectionComponent } from "../../components/features-section/features-section.component";
+import { TestimonialsSectionComponent } from "../../components/testimonials-section/testimonials-section.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, AboutSectionComponent, FeaturesSectionComponent, TestimonialsSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
