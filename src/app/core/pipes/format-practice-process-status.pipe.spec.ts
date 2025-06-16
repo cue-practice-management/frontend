5d117b7288497de1d2992b8d08309ef06ee1d476
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-15 21:28:41 -0500

    feat(shared): add reusable pipes and status badge component

---
+++ src/app/core/pipes/format-practice-process-status.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatPracticeProcessStatusPipe } from './format-practice-process-status.pipe';
+
+describe('FormatPracticeProcessStatusPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatPracticeProcessStatusPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/atoms/status-badge/status-badge.component.ts
@@ -0,0 +1,31 @@
+import { PRACTICE_PROCESS_DELIVERABLE_STATUS_LABELS, PRACTICE_PROCESS_FOLLOW_UP_STATUS_LABELS, PRACTICE_PROCESS_STATUS_LABELS, PracticeProcessDeliverableStatus, PracticeProcessFollowUpStatus, PracticeProcessStatus } from '@/features/practice-process/practice-process.enums';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-status-badge',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-badge.component.html',
+  styleUrl: './status-badge.component.scss'
+})
+export class StatusBadgeComponent {
+  @Input() status!: PracticeProcessStatus | PracticeProcessDeliverableStatus | PracticeProcessFollowUpStatus;
+  @Input() type!: 'practice' | 'deliverable' | 'followUp'; // puedes extender
+  @Input() label?: string;
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+
+  get finalLabel(): string {
+    if (this.label) return this.label;
+
+    switch (this.type) {
+      case 'practice':
+        return PRACTICE_PROCESS_STATUS_LABELS[this.status as PracticeProcessStatus] || this.status;
+      case 'deliverable':
+        return PRACTICE_PROCESS_DELIVERABLE_STATUS_LABELS[this.status as PracticeProcessDeliverableStatus] || this.status;
+      case 'followUp':
+        return PRACTICE_PROCESS_FOLLOW_UP_STATUS_LABELS[this.status as PracticeProcessFollowUpStatus] || this.status;
+      default:
+        return this.status;
+    }
+  }
+}
